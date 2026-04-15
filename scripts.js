fc35535a08901195bb660f583dfc072d9eaddc2a
30197427 <aria.moonsie@ucalgary.ca>
2026-04-15 11:43:44 -0600

    added styling to popup, icon and tried to style desktop wallpaper (didn't work)

--- scripts.js
+++ scripts.js
@@ -4,3 +4,7 @@ const windowEl = document.getElementById("about-window");
 icon.addEventListener("click", () => {//makes whatever visible
     windowEl.style.display = "block";
 });
+
+document.querySelector(".close-button").addEventListener("click", () => {
+    windowEl.style.display="none";
+});
