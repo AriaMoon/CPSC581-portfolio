b8671a0a2f3c1b95f85b2e279be9333816ac3dfa
Aria Moonsie <aria.moonsie@ucalgary.ca>
2026-04-15 17:56:40 -0600

    Made windows draggable + started taskbar

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CPSC581-portfolio
+
+The site is LIVE at https://ariamoon.github.io/CPSC581-portfolio/
--- scripts.js
+++ scripts.js
@@ -8,3 +8,26 @@ icon.addEventListener("click", () => {//makes whatever visible
 document.querySelector(".close-button").addEventListener("click", () => {
     windowEl.style.display="none";
 });
+
+let activeWindow = null;
+let offsetX = 0;
+let offsetY = 0;
+
+document.querySelectorAll(".title-bar").forEach(bar => {
+    bar.addEventListener("mousedown", (e) => {
+        activeWindow = bar.parentElement;
+        offsetX = e.clientX - activeWindow.offsetLeft;
+        offsetY = e.clientY - activeWindow.offsetTop;
+    });
+});
+
+document.addEventListener("mousemove", (e) => {
+    if(activeWindow){
+        activeWindow.style.left = (e.clientX - offsetX) + "px";
+        activeWindow.style.top = (e.clientY - offsetY) + "px";
+    }
+});
+
+document.addEventListener("mouseup", () => {
+    activeWindow = null;
+});
